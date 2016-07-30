a9156434883b6ff5175439445f835b09cf45df53
EwanC <ewan.cr@gmail.com>
2016-07-30 08:20:43 +0100

    Document CLI options in help

--- source/main.cpp
+++ source/main.cpp
@@ -4,23 +4,45 @@
 #include "MrMangler.h"
 #include "FuncDecl.h"
 
+
+void printHelp()
+{
+  std::cout << "Usage: MrMangler [optional args] <input file>" <<std::endl;
+  std::cout << std::endl;
+
+  std::cout << "<input file>       Input file to read from, otherwise "
+               "use stdin." << std::endl;
+  std::cout << std::endl;
+
+  std::cout << "Optional args:" << std::endl;
+  std::cout << "  [-h|--help]      Print this message." <<
+  std::endl;
+
+  std::cout << "  [-i|--itanium]   Use itanium mangling. This is the"
+               " defualt mangling scheme."<< std::endl;
+
+  std::cout << "  [-w|--windows]   Use windows decoration." <<
+  std::endl;
+}
+
 mangle_fn parseArgs(int argc, char** argv, FILE* &input_file)
 {
+    mangle_fn return_fn = &mangle_itanium;
     for(int i=1; i < argc; ++i) {
       if(0 == strcmp(argv[i], "--help") ||
          0 == strcmp(argv[i], "-h")) {
-           std::cout << "help:" <<std::endl;
+           printHelp();
            exit(0);
       }
 
       if(0 == strcmp(argv[i], "--windows") ||
          0 == strcmp(argv[i], "-w")) {
-           return &mangle_windows;
+           return_fn = mangle_windows;
       }
 
       if(0 == strcmp(argv[i], "--Itanium") ||
          0 == strcmp(argv[i], "-i")) {
-          return &mangle_itanium;
+          return_fn = mangle_itanium;
       }
 
       if ('-' != argv[i][0]) {
@@ -32,7 +54,7 @@ mangle_fn parseArgs(int argc, char** argv, FILE* &input_file)
       }
     }
 
-    return &mangle_itanium;
+    return return_fn;
 }
 
 int main(int argc, char** argv)
